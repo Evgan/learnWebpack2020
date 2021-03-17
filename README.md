46ece6053224c69f49913297353e190dcb7e13de
epenkrart <e.penkrat@whiteskydigital.com>
2021-03-18 02:09:04 +0300

    Настройка режимов дев и прод

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ https://youtu.be/eSaF8NXeNsA
 
 ### Docs for webpack:
 https://webpack.js.org/
+
+Остановился на 20:24
+Разобраться почему в прод режим результирующий файл не минимизируется
--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,13 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     mode: 'development',
-    entry: {
+    entry: { // точки входя для webpack
         main: './src/index.js',
         analytics: './src/analytics.js'
     },
-    output: {
+    output: { // куда будет сохранять результат
+        // Так как входных файла два, то используем патерн NAME что бы вебпак автоматом подставил имена от входных файлов
+        // а contenthash в резульирующее имя файла хэш, что бы устранить кэширование
       filename: '[name].[contenthash].js',
       path: path.resolve(__dirname, 'dist')
     },
