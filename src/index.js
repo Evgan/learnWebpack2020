ab505832fb7a43f2e881d37114c683944a40be9d
epenkrart <e.penkrat@whiteskydigital.com>
2021-03-28 01:21:44 +0300

    JSON: вебпак умеет обработать json файлы по умолчанию

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Post from './Post';
 import './styles/styles.css';
+import json from './assets/testJson';
 
 const post = new Post('Мой пост');
 console.log('-----------------------post.toString > ', post.toString());
+console.log('----------------------- json:');
+console.log(json);
