12366a5dc2779222d05336edc98aabede8ae6e4c
epenkrart <e.penkrat@whiteskydigital.com>
2021-04-01 00:18:23 +0300

    почистил код webpack.config.js в соответсвии с ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ const isProd = !isDev;
 const optimization = () => {
     const config = {
         splitChunks: {
-            chunks: "all"
+            chunks: 'all'
         }
     };
     if (isProd) {
@@ -25,18 +25,29 @@ const optimization = () => {
 };
 
 // Что бы хэши были в PROD режиме
-const fileName = ext => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+const fileName = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
 
 // выносим повторяющийся код:
-const cssLoaders = extra => {
+const cssLoaders = (extra) => {
     const loaders = [
         MiniCssExtractPlugin.loader,
-        'css-loader',
+        'css-loader'
     ];
     if (extra) {
         loaders.push(extra);
     }
     return loaders;
+};
+
+// выносим повторяющийся код:
+const babelOptions = (preset) => {
+    const result = {
+        presets: ['@babel/preset-env']
+    };
+    if (preset) {
+        result.presets.push(preset);
+    }
+    return result;
 };
 
 // Что бы eslint прикрутить только в режиме дев
@@ -46,18 +57,7 @@ const jsxLoader = () => {
         options: babelOptions('@babel/preset-react')
     }];
     if (isDev) {
-        result.push('eslint-loader')
-    }
-    return result;
-};
-
-// выносим повторяющийся код:
-const babelOptions = preset => {
-    const result = {
-        presets: ['@babel/preset-env']
-    };
-    if(preset) {
-        result.presets.push(preset);
+        result.push('eslint-loader');
     }
     return result;
 };
@@ -70,40 +70,44 @@ const config = {
         analytics: './analytics.ts'
     },
     output: { // куда будет сохранять результат
-        // Так как входных файла два, то используем патерн NAME что бы вебпак автоматом подставил имена от входных файлов
+        // Так как входных файла два, то используем патерн NAME что бы
+        // вебпак автоматом подставил имена от входных файлов
         // а contenthash в резульирующее имя файла хэш, что бы устранить кэширование
-      // filename: '[name].[contenthash].js',
-      filename: fileName('js'),  // Что бы хэши были в PROD режиме
-      path: path.resolve(__dirname, 'dist')
+        // filename: '[name].[contenthash].js',
+        filename: fileName('js'), // Что бы хэши были в PROD режиме
+        path: path.resolve(__dirname, 'dist')
     },
     resolve: {
-        // extensions: ['.js', '.jsx', '.ts', '.tsx'] Данная настройка позволяет при импорте файла в файл не писать расширения файлов указанных в массиве
+        // extensions: ['.js', '.jsx', '.ts', '.tsx'] Данная настройка позволяет при импорте файла
+        // в файл не писать расширения файлов указанных в массиве
 
         // Очень полезная штука, тип биндим путь к нужным папкам и потом можем юзать из любой вложенности
         // ПРИМЕР, вместо этого: import actionExpl from '../../../../ducks/duckExpl'
         // ПОЛУЧИМ: import actionExpl from '@models/duckExpl'
         alias: {
-            //'@models' : path.resolve(__dirname, 'src/models');
-            //'@ducks': path.resolve(__dirname, 'src/client/redicer/ducks');
-            //'@': path.resolve(__dirname, 'src');
+            // '@models' : path.resolve(__dirname, 'src/models');
+            // '@ducks': path.resolve(__dirname, 'src/client/redicer/ducks');
+            // '@': path.resolve(__dirname, 'src');
         }
     },
     // Оптимизирует финальный бандел, к примеру мы импортируем в двух разных точках входа вешнюю библиотеку jquery,
-    // и если не будет настройки chunks: "all", то для каждой точки входа в их бандлы добавится код этой блилиотеки (два раза одно и тоже будет записано)
-    // Но если есть эта настройка то вебпак умный и этот потворяющий код вынесит в отдельный файли и будет юзать её ссылку в нужных бандлах
+    // и если не будет настройки chunks: "all", то для каждой точки входа в их бандлы добавится код этой блилиотеки
+    // (два раза одно и тоже будет записано)
+    // Но если есть эта настройка то вебпак умный и этот потворяющий код вынесит в отдельный файли и
+    // будет юзать её ссылку в нужных бандлах
     optimization: optimization(),
     devServer: {
-        port: 3005,
+        port: 3005
     },
 
     module: {
         rules: [
             {
-              test: /\.s[ac]ss$/, // [ac] - означает что у нас будет ловить sAss и sCss
-              use: cssLoaders('sass-loader')
+                test: /\.s[ac]ss$/, // [ac] - означает что у нас будет ловить sAss и sCss
+                use: cssLoaders('sass-loader')
             },
             {
-              test: /\.less$/,
+                test: /\.less$/,
                 use: cssLoaders('less-loader')
             },
             {
@@ -132,7 +136,7 @@ const config = {
                 test: /\.m?js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader",
+                    loader: 'babel-loader',
                     options: babelOptions()
                 }
             },
@@ -140,7 +144,7 @@ const config = {
                 test: /\.ts$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader",
+                    loader: 'babel-loader',
                     options: babelOptions('@babel/preset-typescript')
                 }
             },
@@ -165,7 +169,7 @@ const config = {
                 {
                     from: path.resolve(__dirname, 'src/favicon.ico'),
                     to: path.resolve(__dirname, 'dist')
-                },
+                }
             ]
         }),
         new MiniCssExtractPlugin({
